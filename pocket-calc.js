df609cb4386f33338fd2b2df1b61ddeed442b2da
Justin Zimmerman <01001010tz@gmail.com>
2018-12-20 10:50:53 -0500

    Did visual things. Need practically all of the JS.

---
+++ pocket-calc.js
@@ -0,0 +1,68 @@
+function number(number) {
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML += number;
+}
+
+function euler() {
+
+}
+
+function pi() {
+
+}
+
+function xSquared() {
+
+}
+
+function xToTheY() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function allClear() {
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML = "";
+
+}
+
+function negate() {
+
+}
+
+function percent() {
+
+}
+
+function decimal() {
+  let i = 0;
+  i++;
+
+  if(i == 1) {
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML += ".";
+} else {
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML = "SYNTAX ERROR";
+}
+
+}
+
+function equals() {
+
+}
