d54f1c27871784dd4062a128a56e9d6143f71c90
Justin Zimmerman <01001010tz@gmail.com>
2019-01-02 22:06:23 -0500

    Finally got this computer going. Still need to work on JS; troubleshoot HTML and CSS

--- pocket-calc.js
+++ pocket-calc.js
@@ -1,14 +1,17 @@
 function number(number) {
   let bar = document.getElementById("output-bar");
   bar.innerHTML += number;
+
 }
 
 function euler() {
-
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML += "2.71828183";
 }
 
 function pi() {
-
+  let bar = document.getElementById("output-bar");
+  bar.innerHTML += "3.14159265";
 }
 
 function xSquared() {
@@ -52,15 +55,16 @@ function percent() {
 function decimal() {
   let i = 0;
   i++;
+  console.log(i);
 
   if(i == 1) {
   let bar = document.getElementById("output-bar");
   bar.innerHTML += ".";
-} else {
+} /* else {
   let bar = document.getElementById("output-bar");
   bar.innerHTML = "SYNTAX ERROR";
 }
-
+*/
 }
 
 function equals() {
