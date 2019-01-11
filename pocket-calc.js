40c4c6207d3dac97df3135debd735ca1f63d3365
Justin Zimmerman <01001010tz@gmail.com>
2019-01-11 00:06:04 -0500

    Continued to work on JS. Need mostly syntax errors and checks at this point, with other small fixes here and there

--- pocket-calc.js
+++ pocket-calc.js
@@ -1,13 +1,12 @@
-var bar;
-//bar = document.getElementById("output-bar").innerHTML = "0";
+var bar = "";
+bar = document.getElementById("output-bar").innerHTML = "0";
+console.log(bar);
 //Gotta get the zero thing working
 var mymyHistory = "";
 /*Need to add to calculator:
 Scientific notation and commas for numbers that are too large
 Constants that work (problems loading pi and e)
 Multiple operators can be directly added where they shouldn't be able to
-Remove zero from first number after AC
-Consider adding parentheses
 Consider adding default x cubed option
 Consider adding x! using 'for' loop*/
 
@@ -17,11 +16,12 @@ let zeroCheck = false;
 if (bar == 0) {
   zeroCheck = true;
 } console.log(zeroCheck);
+
 if (zeroCheck == true) {
     bar = document.getElementById("output-bar").innerHTML = number;
-} else if (zeroCheck == false){
+  } else if (zeroCheck == false){
   bar = document.getElementById("output-bar").innerHTML += number;
-}
+  }
 }
 
 function euler() {
@@ -60,7 +60,7 @@ function operator(operation) {
 console.log("Second check. Between if statements" + equalsCheck);
 if (equalsCheck === false) {
   myHistory = document.getElementById("myHistory").innerHTML += (bar + operation);
-  bar = document.getElementById("output-bar").innerHTML = "";
+  bar = document.getElementById("output-bar").innerHTML = "0";
   console.log("If clause, line 52, no equals sign");
 } else if (equalsCheck === true) {
     myHistory = document.getElementById("myHistory").innerHTML += (operation);
@@ -77,18 +77,10 @@ function allClear() {
   bar = document.getElementById("output-bar").innerHTML = "0";
   myHistory = document.getElementById("myHistory").innerHTML = "";
 }
-/*PROBLEM: After clearing the 0 doesn't go away. This will become a problem when 0 is made default later. Find a way
- to get rid of the zero when other numbers are put in */
 
 function negate() {
 let equals = eval(bar);
-/*if (equals > 0) {
-  equals -= 2*equals;
-  bar = document.getElementById("output-bar").innerHTML = equals;
-  }else if (equals < 0) {
-  equals += 2*Math.abs(equals)
-  bar = document.getElementById("output-bar").innerHTML = equals;
-}*/
+
   let minusCheck = false; //= myHistory.includes("=");
   console.log("First check");
   let k = bar.length;
@@ -102,13 +94,12 @@ let equals = eval(bar);
 
 if (minusCheck == false) {
   bar = document.getElementById("output-bar").innerHTML = "(-" + bar + ")"
-} else if (minusCheck == true) {
+  } else if (minusCheck == true) {
   bar.replace("(-", "");
   bar.replace(")", "");
   bar = document.getElementById("outupt-bar").innerHTML = bar;
   //Get this part working
-}
-  //May need to sort out some problems here with making numbers negate before totalling. Need to be able to string...
+  }
 }
 
 function percent() {
@@ -117,29 +108,20 @@ bar = document.getElementById("output-bar").innerHTML = equals/100;
 }
 
 function decimal() {
-  let i = 0;
-  i++;
-  console.log(i);
+    let bar = document.getElementById("output-bar").innerHTML += ".";
+  }
 
-  if(i == 1) {
-  let bar = document.getElementById("output-bar").innerHTML += ".";
-} /*else {
-  let bar = document.getElementById("output-bar");
-  bar.innerHTML = "SYNTAX ERROR";
-}*/
 //Problem: Can repeat over and over again without error. Fix that.
-}
+
 
 function equals() {
-
 myHistory = document.getElementById("myHistory").innerHTML += bar;
 let equals = eval(myHistory);
-if (equals > 999999999) {
-  equals = equals.toExponential(8);
-}
+  if (equals > 999999999) {
+    equals = equals.toExponential(8);
+  }
 bar = document.getElementById("output-bar").innerHTML = equals.toLocaleString("en");
 myHistory = document.getElementById("myHistory").innerHTML += "=" + equals;
 //console.log(myHistory);
-//Need to add scientific notation for lengths greater than 9 and commas for non-equals lengths greater than 3
 //Need to add syntax errors in general to make sure this runs right
 }
