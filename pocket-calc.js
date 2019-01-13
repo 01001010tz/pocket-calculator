70e80a734462e6aa9ea073937994dddee22f6012
Justin Zimmerman <01001010tz@gmail.com>
2019-01-13 14:43:39 -0500

    Started finishing syntax stuff and whatnot, now I just need to make sure it works. I have a few things that are giving me console errors. Will probably go in period 6 to fix.

--- pocket-calc.js
+++ pocket-calc.js
@@ -3,41 +3,49 @@ bar = document.getElementById("output-bar").innerHTML = "0";
 console.log(bar);
 //Gotta get the zero thing working
 var myHistory = "";
+var lastChar;
 /*Need to add to calculator:
 Scientific notation and commas for numbers that are too large
 Constants that work (problems loading pi and e)
 Multiple operators can be directly added where they shouldn't be able to
 Consider adding default x cubed option
 Consider adding x! using 'for' loop*/
-function lastCharCheck(){
-    let z = myHistory.length;
-    let lastChar = myHistory[z];
-    console.log("Last history character =" + lastChar);
+
+while (myHistory !== "") {
+  console.log("In while loop. History not empty.")
+let z = myHistory.length;
+lastChar = myHistory[z];
 }
 
 function number(number) {
-lastCharCheck();
-let zeroCheck = false;
+  console.log("Last character" + lastChar);
+  let zeroCheck = false;
 
-if (bar == 0) {
-  zeroCheck = true;
-} console.log(zeroCheck);
+  if (bar == 0) {
+    zeroCheck = true;
+  } console.log(zeroCheck);
 
-if (zeroCheck == true) {
-    bar = document.getElementById("output-bar").innerHTML = number;
-  } else if (zeroCheck == false){
-  bar = document.getElementById("output-bar").innerHTML += number;
-  }
+  if (zeroCheck == true) {
+      bar = document.getElementById("output-bar").innerHTML = number;
+    } else if (zeroCheck == false){
+      bar = document.getElementById("output-bar").innerHTML += number;
+    }
 }
 
 function euler() {
-  lastCharCheck();
-  bar = document.getElementById("output-bar").innerHTML = "(2.718281828)";
+  if (lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar == "*") {
+    bar = document.getElementById("output-bar").innerHTML = "(2.718281828)";
+  } else {
+    alert("SYNTAX ERROR");
+  }
 }
 
 function pi() {
-  lastCharCheck();
-  bar = document.getElementById("output-bar").innerHTML = "(3.141592654)";
+  if (lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar == "*") {
+    bar = document.getElementById("output-bar").innerHTML = "(3.141592654)";
+  } else {
+    alert("SYNTAX ERROR");
+  }
 }
 
 /* 1. I can probably make the constants into one function for efficiency/space
@@ -45,41 +53,45 @@ function pi() {
 for an operator or else they don't work*/
 
 function xSquared() {
-  //need to add history bit here
-  bar = document.getElementById("output-bar").innerHTML = eval(bar)**2;
+myHistory = document.getElementById("myHistory").innerHTML = bar + "**2";
+  bar = document.getElementById("output-bar").innerHTML = "";
 }
 
 function xToTheY() {
-  bar = document.getElementById("output-bar").innerHTML += '**';
+myHistory = document.getElementById("myHistory").innerHTML = bar + "**";
+  bar = document.getElementById("output-bar").innerHTML = "";
 }
 //There's probably a way to consolidate these two functions, if it's easier/more efficient to do that
 
 function operator(operation) {
-  let equalsCheck = false; //= myHistory.includes("=");
-  lastCharCheck();
-  console.log("First check");
-  let k = myHistory.length;
-  for (let i = 0; i < k; i++) {
-    console.log("In loop. Before if check clause");
-    if (myHistory[i] == "=") {
-      equalsCheck = true;
-      console.log(equalsCheck);
+if (lastChar != "+" && lastChar != "-" && lastChar != "/" && lastChar != "*" && lastChar != NaN) {
+    console.log("Last character" + lastChar);
+    let equalsCheck = false;
+    let k = myHistory.length;
+    for (let i = 0; i < k; i++) {
+      console.log("In loop. Before if check clause");
+      if (myHistory[i] == "=") {
+        equalsCheck = true;
+        console.log(equalsCheck);
+      }
     }
-  }
-console.log("Second check. Between if statements" + equalsCheck);
-if (equalsCheck === false) {
-  myHistory = document.getElementById("myHistory").innerHTML += (bar + operation);
-  bar = document.getElementById("output-bar").innerHTML = "0";
-  console.log("If clause, line 52, no equals sign");
-} else if (equalsCheck === true) {
-    myHistory = document.getElementById("myHistory").innerHTML += (operation);
-    bar = document.getElementById("output-bar").innerHTML = "";
-    let equalsPlace = myHistory.indexOf("=");
-    let newHistory = myHistory.substring((equalsPlace + 1));
-    myHistory = document.getElementById("myHistory").innerHTML = newHistory;
-    console.log("Else if clause, line 59, equals sign");
-  }
+  console.log("Second check. Between if statements" + equalsCheck);
+  if (equalsCheck === false) {
+    myHistory = document.getElementById("myHistory").innerHTML += (bar + operation);
+    bar = document.getElementById("output-bar").innerHTML = "0";
+    console.log("If clause, line 52, no equals sign");
+  } else if (equalsCheck === true) {
+      myHistory = document.getElementById("myHistory").innerHTML += (operation);
+      bar = document.getElementById("output-bar").innerHTML = "0";
+      let equalsPlace = myHistory.indexOf("=");
+      let newHistory = myHistory.substring((equalsPlace + 1));
+      myHistory = document.getElementById("myHistory").innerHTML = newHistory;
+      console.log("Else if clause, line 59, equals sign");
+    }
   //PROBLEM: Operators can be used twice in a row. Find a way to render a syntax error
+} else {
+  alert("SYNTAX ERROR");
+  }
 }
 
 function allClear() {
@@ -88,8 +100,6 @@ function allClear() {
 }
 
 function negate() {
-let equals = eval(bar);
-
   let minusCheck = false; //= myHistory.includes("=");
   console.log("First check");
   let k = bar.length;
@@ -117,21 +127,26 @@ bar = document.getElementById("output-bar").innerHTML = equals/100;
 }
 
 function decimal() {
-    lastCharCheck();
+  if (lastChar != ".") {
     let bar = document.getElementById("output-bar").innerHTML += ".";
+  } else {
+    alert("SYNTAX ERROR");
   }
-
+}
 //Problem: Can repeat over and over again without error. Fix that.
 
-
 function equals() {
-myHistory = document.getElementById("myHistory").innerHTML += bar;
-let equals = eval(myHistory);
-  if (equals > 999999999) {
-    equals = equals.toExponential(8);
-  }
-bar = document.getElementById("output-bar").innerHTML = equals.toLocaleString("en");
-myHistory = document.getElementById("myHistory").innerHTML += "=" + equals;
+if (lastChar != "+" && lastChar != "-" && lastChar != "/" && lastChar != "*" && lastChar != NaN) {
+    myHistory = document.getElementById("myHistory").innerHTML += bar;
+  let equals = eval(myHistory);
+    if (equals > 999999999) {
+      equals = equals.toExponential(8);
+    }
+  bar = document.getElementById("output-bar").innerHTML = equals.toLocaleString("en");
+  myHistory = document.getElementById("myHistory").innerHTML += "=" + equals;
+} else {
+  alert("SYNTAX ERROR");
+}
 //console.log(myHistory);
 //Need to add syntax errors in general to make sure this runs right
 }
