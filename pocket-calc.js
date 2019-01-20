9a91e36e5f1f6c5ad3de1563fd5ed479ca9da5a6
Justin Zimmerman <01001010tz@gmail.com>
2019-01-20 14:00:25 -0500

    Took another crack at commas again (first try now commented out). Still not working, because JS still doesn't like .replace(); Big rip that sucks I guess

--- pocket-calc.js
+++ pocket-calc.js
@@ -39,14 +39,21 @@ if (lastIsConstant == false) {
   if (zeroCheck == true) {
       bar = document.getElementById("output-bar").innerHTML = number;
     } else if (zeroCheck == false){
-        console.log("Bar check  0 part 2, shouldn't have any problems yet " + bar);
+        /*console.log("Bar check  0 part 2, shouldn't have any problems yet " + bar);
         console.log("Bar check 1, should have commas " + bar);
         let temp = bar.replace(/,/g, );
         console.log("Bar check 2, should not have commas " + bar);
         temp += number;
         console.log("Bar check 3, should not have commas and should have next number " + bar);
+        console.log(temp)
         bar.innerHTML = temp.toLocaleString("en");
-        console.log("Bar check 4, should be full number with commas " + bar);
+        console.log("Bar check 4, should be full number with commas " + bar);*/
+        bar = document.getElementById("output-bar").innerHTML;
+        let temp = bar.innerHTML;
+        bar.innerHTML = "";
+        temp = temp.replace(/,/g, "")
+        temp += number;
+        bar = document.getElementById("output-bar").innerHTML = temp.toLocaleString("en");
        if (bar.length > 9) {
         //scientific notation here
       }
